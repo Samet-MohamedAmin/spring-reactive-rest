2383c04208f16355a78ac3ee6b8c9e2c74a6d3f6
MohamedAmin Samet <mohamedamin.samet@yandex.com>
2019-09-18 13:01:41 +0100

    ignore idEmpty field

--- src/main/java/samet/spring/reactiverest/models/BaseEntity.java
+++ src/main/java/samet/spring/reactiverest/models/BaseEntity.java
@@ -1,11 +1,13 @@
 package samet.spring.reactiverest.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public interface BaseEntity {
 
     String getId();
     void setId(String id);
 
+    @JsonIgnore
     default boolean isIdEmpty(){
         if(getId() == null){
             return true;
